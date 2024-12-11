258ea83d6643529061e70bcb489aa532565f7127
Komecitos <irga.prayoga273@gmail.com>
2024-12-11 22:38:07 +0700

    login page ditambahkan

--- app/src/main/java/com/example/momen_kopi/MainActivity.java
+++ app/src/main/java/com/example/momen_kopi/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.momen_kopi;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button masuk;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.login_page);
+
+        masuk = findViewById(R.id.loginButton);
+        masuk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this, )
+            }
         });
+
     }
 }
